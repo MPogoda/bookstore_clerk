b47109161b3aaf06e91394b71227466ee8adc45d
Michael Pogoda <do0dlez1991@gmail.com>
2013-03-19 02:45:03 +0200

    Added RAII-class DBOpener. Do not store sql queries as class members anymore

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -16,12 +16,45 @@
 #include "logindialog.h"
 #include "fillrequestdialog.h"
 
-namespace {
-struct DebugHelper {
+namespace
+{
+/**
+ * @brief The DebugHelper struct is RAII-helper that helps to print Q_FUNC_INFO whenever you enter
+ * function (if you've declared object of that type at beginning) and when you leave it.
+ */
+struct DebugHelper
+{
     const QString m_funcInfo;
-    DebugHelper( const QString funcInfo ) : m_funcInfo(funcInfo)
-    { qDebug() << "====ENTERING " << funcInfo << "========="; }
-    ~DebugHelper() { qDebug() << "=====LEAVING " << m_funcInfo << "===========";}
+    DebugHelper( const QString funcInfo )
+        : m_funcInfo(funcInfo)
+    {
+        qDebug() << "=====ENTERING " << m_funcInfo << "===========";
+    }
+    ~DebugHelper()
+    {
+        qDebug() << "=====LEAVING  " << m_funcInfo << "===========";
+    }
+};
+
+/**
+ * @brief The DBOpener struct RAII helper that tries to (re)open connection database and closes it afterwards.
+ */
+struct DBOpener
+{
+    MainWindow * const mw;
+    DBOpener( MainWindow * const iMW)
+        : mw( iMW )
+    {
+        bool isOpened = QSqlDatabase::database().open();
+        qDebug() << "DBOpen: " << isOpened;
+        if (!isOpened)
+            QMessageBox::critical(iMW, "Database connection error", "Cannot establish connection to database");
+    }
+    ~DBOpener()
+    {
+        qDebug() << "~DBOpen: ";
+        QSqlDatabase::database().close();
+    }
 };
 }
 
@@ -63,23 +96,19 @@ MainWindow::MainWindow(QWidget *parent)
 
     connect( ui->filterButton, SIGNAL(clicked()), this, SLOT(redrawView()));
     connect( fillRequestAction, SIGNAL(triggered()), this, SLOT(fillRequest()));
+
+    connect( ui->actionDisconnect, SIGNAL(triggered()), this, SLOT(disconnect_clerk()) );
+    connect( ui->actionReconnect, SIGNAL(triggered()), this, SLOT(processLogin()) );
+    connect( this, SIGNAL(connected()), this, SLOT(connect_clerk()) );
 }
-
-
 
 MainWindow::~MainWindow()
 {
     DebugHelper debugHelper( Q_FUNC_INFO);
     delete ui;
-    delete m_simpleSearch;
-    delete m_searchBook;
-    delete m_searchAuthors;
-    delete m_searchSold;
-    delete m_searchPasswordHash;
-    delete m_insertRequest;
 }
 
-void MainWindow::setup_connection()
+void MainWindow::setup_connection() const
 {
     DebugHelper debugHelper( Q_FUNC_INFO);
     QSettings settings( "settings.ini", QSettings::IniFormat );
@@ -105,70 +134,115 @@ void MainWindow::setup_connection()
     db.setPassword(     password );
 }
 
-void MainWindow::prepare_queries()
+namespace
 {
-    DebugHelper debugHelper( Q_FUNC_INFO);
-    m_simpleSearch = new QSqlQuery;
-  //  m_simpleSearch->setForwardOnly( true );
-    qDebug() << "SimpleSearch" <<
-    m_simpleSearch->prepare("select   book.isbn as ISBN, count(*) as Sold, max(quantity) as Quantity "
-                            "from     history_of_purchasing join book on book.isbn = history_of_purchasing.isbn "
-                            "where    (purchasing_date >= :fromDate and purchasing_date <= :toDate) "
-                            "     and (quantity >= :fromStock and quantity <= :toStock) "
-                            "group by book.isbn "
-                            "having   count(*) >= :fromBought and count(*) <= :toBought"
-                            );
-    m_simpleSearch->bindValue(":fromDate", "0001-01-01");
-    m_simpleSearch->bindValue(":toDate", "3000-12-12");
-    m_simpleSearch->bindValue(":fromStock", 0);
-    m_simpleSearch->bindValue(":toStock", 9000);
-    m_simpleSearch->bindValue(":fromBought", 0);
-    m_simpleSearch->bindValue(":toBought", 9010);
+/**
+ * @brief find_request_for_book checks whether there is request for that book already
+ * @param isbn ISBN number of book
+ * @return true if exists
+ */
+bool find_request_for_book(const QString& isbn )
+{
+    DebugHelper debugHelper( Q_FUNC_INFO );
+    // assume db is opened
+    QSqlQuery searchRequest;
+    searchRequest.setForwardOnly( true );
+    qDebug() << "Prepare: " <<
+                searchRequest.prepare( "select count(*) from request where isbn = :isbn" );
+    searchRequest.bindValue( ":isbn", isbn );
+    qDebug() << "Exec: " << searchRequest.exec() << searchRequest.first();
 
-    m_searchBook = new QSqlQuery;
-    m_searchBook->setForwardOnly( true );
-    qDebug() << "Search Book" <<
-    m_searchBook->prepare( "select title, quantity from book where isbn = :isbn" );
-
-    m_searchAuthors = new QSqlQuery;
-    m_searchAuthors->setForwardOnly( true );
-    qDebug() << "Search Authors" <<
-    m_searchAuthors->prepare( "select name "
-                              "from book join book_s_author "
-                                        "on book_s_author.isbn = book.isbn "
-                                    "join author "
-                                        "on author.author_id = book_s_author.author_id "
-                              "where book_s_author.isbn = :isbn ");
-
-    m_searchSold = new QSqlQuery;
-    m_searchSold->setForwardOnly( true );
-    qDebug() << "Search Sold" <<
-    m_searchSold->prepare( "select count(*) as sold "
-                           "from history_of_purchasing join book "
-                                                      "on book.isbn = history_of_purchasing.isbn "
-                           "where purchasing_date > :fromDate "
-                                  "and book.isbn = :isbn "
-                           "group by book.isbn " );
-    m_searchSold->bindValue( ":fromDate", "0001-01-01" );
-
-    m_searchPasswordHash = new QSqlQuery;
-    m_searchPasswordHash->setForwardOnly( true );
-    qDebug() << "Search Password Hash" <<
-    m_searchPasswordHash->prepare( "select count(*) "
-                                   "from clerk "
-                                   "where clerk_id = :clerkID "
-                                          "and password_hash = :passwordHash ");
-
-    m_insertRequest = new QSqlQuery;
-    qDebug() << "Insert Request: " <<
-    m_insertRequest->prepare( "insert into request( isbn, quantity, clerk_id ) values "
-                                    "( :isbn, :quantity, :clerkID )");
+    return (0 != searchRequest.value( 0 ).toUInt());
 }
 
+/**
+ * @brief find_books_title_and_quantity finds book's title and available quantity
+ * @param isbn  ISBN number of book
+ * @param title found book's title
+ * @param quantity found book's quantity
+ */
+void find_books_title_and_quantity( const QString& isbn, QString& title, uint& quantity)
+{
+    DebugHelper debugHelper( Q_FUNC_INFO );
+    QSqlQuery searchBook;
+    searchBook.setForwardOnly( true );
+    qDebug() << "Prepare: " <<
+                searchBook.prepare( "select title, quantity from book where isbn = :isbn" );
+
+    searchBook.bindValue( ":isbn", isbn );
+
+    qDebug() << "Exec: " << searchBook.exec() << searchBook.first();
+
+    title = searchBook.value( 0 ).toString();
+    quantity = searchBook.value( 1 ).toUInt();
+    qDebug() << "Title: " << title << "; Quantity: " << quantity;
+}
+
+/**
+ * @brief find_authors_for_book get all authors that have written book
+ * @param isbn ISBN number of book
+ * @return StringList of authors
+ */
+const QStringList find_authors_for_book( const QString& isbn )
+{
+    DebugHelper debugHelper( Q_FUNC_INFO );
+    QSqlQuery searchAuthors;
+    searchAuthors.setForwardOnly( true );
+    qDebug() << "Prepare: " <<
+        searchAuthors.prepare( "select name "
+                               "from book join book_s_author "
+                                         "on book_s_author.isbn = book.isbn "
+                                         "join author "
+                                         "on author.author_id = book_s_author.author_id "
+                               "where book_s_author.isbn = :isbn ");
+
+    searchAuthors.bindValue( ":isbn", isbn );
+    qDebug() << "Exec: " << searchAuthors.exec();
+
+    QStringList authors;
+    while (searchAuthors.next())
+        authors << searchAuthors.value( 0 ).toString();
+
+    qDebug() << "Authors: " << authors;
+
+    return authors;
+}
+
+/**
+ * @brief find_sold_number finds how many books with that ISBN have been sold past week (TODO)
+ * @param isbn ISBN number of book
+ * @return number of succesful purchases for past week
+ */
+uint find_sold_number( const QString& isbn)
+{
+    DebugHelper debugHelper( Q_FUNC_INFO );
+    QSqlQuery searchSold;
+    searchSold.setForwardOnly( true );
+    qDebug() << "Prepare: " <<
+                searchSold.prepare( "select count(*) as sold "
+                                    "from history_of_purchasing join book "
+                                    "on book.isbn = history_of_purchasing.isbn "
+                                    "where purchasing_date > :fromDate "
+                                    "and book.isbn = :isbn "
+                                    "group by book.isbn " );
+
+    // TODO: bind week before current
+    searchSold.bindValue( ":fromDate", "0001-01-01" );
+    searchSold.bindValue( ":isbn", isbn );
+
+    qDebug() << "Exec: " << searchSold.exec() << searchSold.first();
+
+    const uint sold = searchSold.value( 0 ).toUInt();
+    qDebug() << "Sold: " << sold;
+
+    return sold;
+}
+}
 
 void MainWindow::fillRequest()
 {
     DebugHelper debugHelper( Q_FUNC_INFO);
+    DBOpener    dbopener( this );
 
     const int row = m_inputSelectionModel->currentIndex().row();
     if (-1 == row)
@@ -179,69 +253,113 @@ void MainWindow::fillRequest()
 
     const QString isbn = m_inputModel->record( row ).value( "isbn" ).toString();
     qDebug() << "ISBN: " << isbn;
-    m_searchBook->bindValue( ":isbn", isbn );
-    qDebug() << "1st query: " << m_searchBook->exec() << m_searchBook->first();
 
-    const QString title = m_searchBook->value( 0 ).toString();
-    const uint quantity = m_searchBook->value( 1 ).toUInt();
-    qDebug() << title << quantity;
+    //check if there already request for that ISBN
+    if ( find_request_for_book( isbn ) )
+    {
+        qDebug() << "Found request :<";
+        QMessageBox::information( this, tr("Cannot add request"), tr("Request already exists! Contact administrator"));
+        return;
+    }
 
+    QString title;
+    uint quantity;
+    find_books_title_and_quantity( isbn, title, quantity);
 
-    m_searchAuthors->bindValue( ":isbn", isbn );
-    qDebug() << "2nd query: " << m_searchAuthors->exec();
-    QStringList authors;
-    while (m_searchAuthors->next())
-        authors << m_searchAuthors->value( 0 ).toString();
+    const QStringList authors = find_authors_for_book( isbn );
 
-    qDebug() << "authors: " << authors;
-
-    m_searchSold->bindValue( ":isbn", isbn );
-    qDebug() << "3rd query: " << m_searchSold->exec() << m_searchSold->first();
-    const uint sold = m_searchSold->value( 0 ).toUInt();
-
-    qDebug() << "sold: " << sold;
+    const uint sold = find_sold_number( isbn );
 
     m_fillRequest->prepareForm( isbn, title, authors.join(", "), quantity, sold);
     if (QDialog::Accepted != m_fillRequest->exec())
     {
-        qDebug() << "CANCELLED";
+        qDebug() << "Request has been cancelled!";
         return;
     }
 
     const uint request = m_fillRequest->quantity();
 
-    m_insertRequest->bindValue( ":isbn", isbn );
-    m_insertRequest->bindValue( ":quantity", request );
-    m_insertRequest->bindValue( ":clerkID", m_clerkID );
+    QSqlQuery insertRequest;
+    qDebug() << "Prepare: " <<
+                insertRequest.prepare( "insert into request( isbn, quantity, clerk_id ) values "
+                                       "( :isbn, :quantity, :clerkID )");
 
-    qDebug() << "TRANSACTION: " <<
-    QSqlDatabase::database().transaction();
-    qDebug() << "INSERT : " <<
-                m_insertRequest->exec();
-    qDebug() << "COMMIT: " <<
+    insertRequest.bindValue( ":isbn", isbn );
+    insertRequest.bindValue( ":quantity", request );
+    insertRequest.bindValue( ":clerkID", m_clerkID );
+
+    qDebug() << "Transaction: " <<
+                QSqlDatabase::database().transaction();
+    qDebug() << "Exec: " <<
+                insertRequest.exec();
+    qDebug() << "Commit: " <<
                 QSqlDatabase::database().commit();
-    // TODO: prepare insert query.
-    // TODO: clerkID...
 }
 
+void MainWindow::disconnect_clerk()
+{
+    DebugHelper debugHelper( Q_FUNC_INFO );
+
+    m_inputModel->clear();
+    ui->tabWidget->setEnabled( false );
+    ui->mainToolBar->setEnabled( false );
+    ui->filterGroupBox->hide();
+    ui->filterToggleButton->setEnabled( false );
+    ui->boughtLessThanBox->setChecked( false );
+    ui->boughtMoreThanBox->setChecked( false );
+    ui->boughtLessThanBox->setChecked( false );
+    ui->boughtMoreThanBox->setChecked( false );
+
+    ui->actionDisconnect->setEnabled( false );
+
+    m_clerkID = 0;
+}
+
+void MainWindow::connect_clerk()
+{
+    DebugHelper debugHelper( Q_FUNC_INFO );
+
+    if (0 == m_clerkID)
+    {
+        qDebug() << "Not connected.";
+        return;
+    }
+
+    ui->tabWidget->setEnabled( true );
+    ui->mainToolBar->setEnabled( true );
+    ui->filterToggleButton->setEnabled( true );
+
+    ui->actionDisconnect->setEnabled( true );
+}
 
 void MainWindow::processLogin()
 {
     DebugHelper debugHelper( Q_FUNC_INFO);
-    ui->tabWidget->setEnabled(false);
+
+    disconnect_clerk();
+
     m_login->clear();
     if (QDialog::Accepted == m_login->exec())
     {
-        // TODO: clear all tables, make initial state
-        m_inputModel->clear();
+        DBOpener dbopener( this );
 
         qDebug() << "Trying to login with ID: " << m_login->userName() << "; and passwordHash = " <<
                     m_login->passwordHash();
-        m_searchPasswordHash->bindValue( ":clerkID", m_login->userName() );
-        m_searchPasswordHash->bindValue( ":passwordHash", m_login->passwordHash() );
-        qDebug() << "Executing password hash query: " << m_searchPasswordHash->exec()
-                                                      << m_searchPasswordHash->first();
-        if (1 == m_searchPasswordHash->value( 0 ).toUInt())
+
+        QSqlQuery searchPasswordHash;
+        searchPasswordHash.setForwardOnly( true );
+        qDebug() << "Prepare: " <<
+                    searchPasswordHash.prepare( "select count(*) "
+                                                "from clerk "
+                                                "where clerk_id = :clerkID "
+                                                "and password_hash = :passwordHash ");
+        searchPasswordHash.bindValue( ":clerkID", m_login->userName() );
+        searchPasswordHash.bindValue( ":passwordHash", m_login->passwordHash() );
+
+        qDebug() << "Exec: " << searchPasswordHash.exec()
+                 << searchPasswordHash.first();
+
+        if (1 == searchPasswordHash.value( 0 ).toUInt())
         {
             m_clerkID = m_login->userName().toUInt();
             emit connected();
@@ -263,45 +381,50 @@ void MainWindow::processLogin()
 void MainWindow::redrawView()
 {
     DebugHelper debugHelper( Q_FUNC_INFO);
-    //    if (NULL == m_inputModel)
-    //        m_inputModel = new QSqlQueryModel( this );
-    //    if (NULL != m_inputModel)
-    //    {
-    //        ui->tableView->setModel(NULL);
-    //        delete m_inputModel;
-    //    }
-    //    m_inputModel = new QSqlQueryModel(this);
-    m_simpleSearch->bindValue(":fromBought",
-                              ui->boughtMoreThanBox->isChecked() ?
-                                  ui->boughtMoreThenSpin->value()
-                                : 0
-                                  );
-    m_simpleSearch->bindValue(":toBought",
-                              ui->boughtLessThanBox->isChecked() ?
-                                  ui->boughtLessThenSpin->value()
-                                : 9000
-                                  );
-    m_simpleSearch->bindValue(":fromStock",
-                              ui->instockMoreThanBox->isChecked() ?
-                                  ui->instockMoreThenSpin->value()
-                                : 0
-                                  );
-    m_simpleSearch->bindValue(":toStock",
-                              ui->instockLessThanBox->isChecked() ?
-                                  ui->instockLessThenSpin->value()
-                                : 9000
-                                  );
+    DBOpener dbopener( this );
 
-    qDebug() << m_simpleSearch->exec();
+    QSqlQuery simpleSearch;
+    qDebug() << "Prepare: " <<
+                simpleSearch.prepare("select   book.isbn as ISBN, count(*) as Sold, max(quantity) as Quantity "
+                                     "from     history_of_purchasing join book on book.isbn = history_of_purchasing.isbn "
+                                     "where    (purchasing_date >= :fromDate and purchasing_date <= :toDate) "
+                                     "     and (quantity >= :fromStock and quantity <= :toStock) "
+                                     "group by book.isbn "
+                                     "having   count(*) >= :fromBought and count(*) <= :toBought"
+                                     );
+    // TODO: bind past week date
+    simpleSearch.bindValue(":fromDate", "0001-01-01");
+    simpleSearch.bindValue(":toDate", "3000-12-12");
 
-    statusBar()->showMessage(tr("%1 row(s) were found.").arg(m_simpleSearch->size()));
+    simpleSearch.bindValue(":fromBought",
+                           ui->boughtMoreThanBox->isChecked() ?
+                               ui->boughtMoreThenSpin->value()
+                             : 0
+                               );
+    simpleSearch.bindValue(":toBought",
+                           ui->boughtLessThanBox->isChecked() ?
+                               ui->boughtLessThenSpin->value()
+                             : 9000
+                               );
+    simpleSearch.bindValue(":fromStock",
+                           ui->instockMoreThanBox->isChecked() ?
+                               ui->instockMoreThenSpin->value()
+                             : 0
+                               );
+    simpleSearch.bindValue(":toStock",
+                           ui->instockLessThanBox->isChecked() ?
+                               ui->instockLessThenSpin->value()
+                             : 9000
+                               );
 
-    m_inputModel->setQuery(*m_simpleSearch);
+    qDebug() << "Exec: " << simpleSearch.exec();
 
-    ui->tabWidget->setEnabled(true);
+    statusBar()->showMessage(tr("%1 row(s) were found.").arg(simpleSearch.size()));
+
+    m_inputModel->setQuery(simpleSearch);
 }
 
-void MainWindow::connect_filters()
+void MainWindow::connect_filters() const
 {
     connect(ui->boughtLessThenSpin, SIGNAL(valueChanged(int)), this, SLOT(boughtLessTrigger(int)));
     connect(ui->boughtMoreThenSpin, SIGNAL(valueChanged(int)), this, SLOT(boughtMoreTrigger(int)));
@@ -314,7 +437,7 @@ void MainWindow::connect_filters()
     connect( ui->instockMoreThanBox, SIGNAL(clicked(bool)), this, SLOT(instockMoreBoxTrigger(bool)));
 }
 
-void MainWindow::disconnect_filters()
+void MainWindow::disconnect_filters() const
 {
     disconnect( ui->boughtLessThenSpin, SIGNAL(valueChanged(int)),this, SLOT(boughtLessTrigger(int)) );
     disconnect( ui->boughtMoreThenSpin, SIGNAL(valueChanged(int)),this, SLOT(boughtMoreTrigger(int)) );
@@ -411,7 +534,6 @@ void MainWindow::filterChanged(const int buttonId)
 
     if (2 != buttonId)
         connect_filters();
-
 }
 
 
--- mainwindow.h
+++ mainwindow.h
@@ -9,7 +9,6 @@ class MainWindow;
 class LoginDialog;
 class FillRequestDialog;
 class QSqlQueryModel;
-class QSqlQuery;
 class QButtonGroup;
 class QItemSelectionModel;
 
@@ -18,35 +17,63 @@ class MainWindow : public QMainWindow
     Q_OBJECT
     
 public:
-    explicit MainWindow(QWidget *parent = 0);
+    explicit MainWindow(QWidget *parent = NULL);
     ~MainWindow();
     
 private:
     Ui::MainWindow *ui;
-    uint           m_clerkID;
-    LoginDialog    *m_login;
+    /**
+     * @brief m_clerkID ID number of connected clerk
+     */
+    uint m_clerkID;
+    /**
+     * @brief m_login Login dialog form
+     */
+    LoginDialog *m_login;
+    /**
+     * @brief m_fillRequest Fill Request form
+     */
     FillRequestDialog *m_fillRequest;
+    /**
+     * @brief m_inputModel Model that will hold data for input view
+     */
     QSqlQueryModel *m_inputModel;
+    /**
+     * @brief m_inputSelectionModel Selection model for m_inputModel
+     */
     QItemSelectionModel *m_inputSelectionModel;
-    QSqlQuery      *m_simpleSearch;
-    QSqlQuery      *m_searchBook;
-    QSqlQuery      *m_searchAuthors;
-    QSqlQuery      *m_searchSold;
-    QSqlQuery      *m_searchPasswordHash;
-    QSqlQuery      *m_insertRequest;
-    QButtonGroup   *m_filterButtons;
+    /**
+     * @brief m_filterButtons group of radio buttons for filters
+     */
+    QButtonGroup *m_filterButtons;
 
     /**
      * @brief Setup database connection: login, host, etc
      */
-    void setup_connection();
+    void setup_connection() const;
 
-    void prepare_queries();
-    void disconnect_filters();
-    void connect_filters();
+    /**
+     * @brief disconnect_filters disconnects all signals from filter controls
+     */
+    void disconnect_filters() const;
+    /**
+     * @brief connect_filters connects all signals back to filter controls
+     */
+    void connect_filters() const;
 private slots:
+    /**
+     * @brief processLogin (re)login into system
+     * asks login information (clerkID & password) and tries to find such credentials in DB
+     */
     void processLogin();
+    /**
+     * @brief redrawView Run again select query (possibly with new parameters) and show results in main view
+     */
     void redrawView();
+
+    /**
+     * @brief Dummy slots that will maintain filter controls in usable state
+     */
     void boughtLessTrigger(const int val);
     void boughtLessBoxTrigger(const bool isOn );
     void boughtMoreTrigger(const int val);
@@ -56,10 +83,26 @@ private slots:
     void instockMoreTrigger(const int val);
     void instockMoreBoxTrigger(const bool val);
 
+    /**
+     * @brief filterChanged do some action when filter has changed
+     * @param buttonId number of filter selected
+     */
     void filterChanged(const int buttonId);
 
+    /**
+     * @brief fillRequest Show information about selected entry and asks about how many books to request
+     */
     void fillRequest();
+
+    /**
+     * @brief disconnect_clerk Disconnect current clerk (clear all tables, etc, etc)
+     */
+    void disconnect_clerk();
+
+    /**
+     * @brief connect_clerk enables all widgets after succesful login of clerk
+     */
+    void connect_clerk();
 signals:
     void connected();
 };
-
